e5826b4cba177bcb2b3530aa04d9f3aad2c23cb5
#nombre <luis.moreno@gcloud.ua.es>
2020-11-19 19:04:42 +0100

    Cambios

--- 3. Regresión lineal/2. Regresión lineal múltiple.R
+++ 3. Regresión lineal/2. Regresión lineal múltiple.R
@@ -2,30 +2,52 @@
 #MODELO DE REGRESIÓN LINEAL MÚLTIPLE#
 #####################################
 
-
-V1~V2+V3+V4
-
-
-#Coeficiente de correlación
-cor(BaseDatos)
-
-NuevaBaseDatos <- select(BaseDatos, V1, V2, V3, V4)
-
-
-#Diagramas de dispersión de conjunto de variables
-pairs(#formula)
-  pairs(V1~V2+V3+V4, main='Gráficos de dispersión', col='blue', col.axis='black')
+#--------------------------------
   
+#Definimos el modelo: V1~V2+V3+V4+V5
   
+#--------------------------------  
   
-  #1. Obtener los estadísticos de la muestra
+#Análisis exploratorio
+  head(BaseDatos)
+  summary(BaseDatos)
+
+#---------¡PROBLEMAS!-----------------------
+
+  #Posible correlación de predictores (colinealidad)
+  #Variables que no aportan nada al modelo (parsimonia)
+  #Relación lineal
+  #Distribución normal de los residuos
+  #Homocedasticidad (Varianza de los residuos constante)
+  #Grados de libertad insuficientes (observaciones > 10*variables)
+
+#-------------------Correlación entre variables-------------------------
+  
+#Base de Datos
+  #library("dplyr")  
+  NuevaBaseDatos <- select(BaseDatos, V1, V2, V3, V4, V5)
+  
+  #Coeficiente de correlación
+  #Valores cercanos a 1 = alta relación entre las variables
+  cor(NuevaBaseDatos)
+
+  #Diagramas de dispersión de conjunto de variables
+  pairs(#formula)
+  pairs(V1~V2+V3+V4+V5, main='Gráficos de dispersión', col='blue', col.axis='black')
+  
+  reg2 <- lm(V1~V2+V3+V4+V5)
+  summary(reg2)
+
+  
+  V1 <- BaseDatos$OcupHot
+  V2 <- log(BaseDatos$GDPpc)
+  V3 <- log(BaseDatos$MovA) #Eliminar 0
+  V4 <- log(BaseDatos$ABOccuRate)
+  V5 <- BaseDatos$S.S  
   
   V1 <- BaseDatos$OcupHot
   V2 <- BaseDatos$GDPpc
   V3 <- BaseDatos$MovA
-  V4 <- BaseDatos$S.S
+  V4 <- BaseDatos$ABOccuRate
+  V5 <- BaseDatos$S.S  
   
-  
-  #Estimación de un modelo de regresión lineal múltiple:
-  reg2 <- lm(V1~V2+V3+V4)
-  summary(reg2)
---
+++ 5. Naive Bayes/Naive Bayes.R
@@ -0,0 +1,9 @@
+#############
+#NAIVE BAYES#
+#############
+
+
+
+#https://rpubs.com/mao045/485540
+#https://fervilber.github.io/Aprendizaje-supervisado-en-R/ingenuo.html
+#https://www.aprendemachinelearning.com/comprar-casa-o-alquilar-naive-bayes-usando-python/
