3ba581ab60939c4b9dc063f379c864d51f8a9400
luis moreno <72371566+luismor85@users.noreply.github.com>
2020-11-23 17:56:56 +0100

    Funciones básicas

--- 0. Conectar github/0. Funciones básicas.R
+++ 0. Conectar github/0. Funciones básicas.R
@@ -10,3 +10,43 @@
   
 #Comenzar a utilizar un paquete instalado
   library(paquete a usar)
+  
+#Github como calculadora
+  #Códigos básicos: +; -; /; ^; decimal = .
+          #%/% = cociente entero
+          #pi = código pi
+          #Inf = infinito
+          #NA = Not availeable
+          #Nan = Not a number
+          #e+10 = 0.0000000... tantos ceros como el número indique
+  
+    #Ejemplos
+  
+    2.3+1.2345
+    2+(3+1); 2^3
+    8/5
+    (8%/%5)
+    pi; pi^2
+    Inf; -Inf
+    5/0 #Infinito
+    0/0 #No tiene resultado numérico
+    2^50
+
+  #Funciones
+    
+    sqrt(x) #raíz cuadrada
+    exp(x) #e elevado a x
+    log(x) #logaritmo neperiano de x
+    log10(x) #logaritmo de x en base 10
+    abs(x) #número absoluto de x
+    factorial(x) #factorial n*(n-1)
+    choose(n, m) #coeficiente binomial (factn / m-(factn-factm))
+      #Tengo 5 platos (n) y quiero coger 3: ¿cuántas posibilidades distintas tengo?
+      #el plato 1, 2 y 3, el 1, 3 y 4, el 1, 5, 7... 10 formas
+      choose (5, 3)
+      
+  #Trigonometría
+      
+      sin(x) #seno de x
+      cos(x) #coseno de x
+      tan(x) #tangente de x
