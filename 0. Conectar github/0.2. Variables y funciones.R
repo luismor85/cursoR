5ed505399447d9756d3775f3bbf2adb3c64bb48b
#nombre <luis.moreno@gcloud.ua.es>
2020-11-24 07:32:01 +0100

    0.2 variables y funciones

---
+++ 0. Conectar github/0.2. Variables y funciones.R
@@ -0,0 +1,29 @@
+#######################
+#VARIABLES Y FUNCIONES#
+#######################
+
+#Crear variables
+  nombre de variables =/<- función
+  x = (pi^2)/2
+  y <- "Antonio"
+  
+#Crear funciones
+  nombre de la funcion <- crear una función con una x
+  
+  f <- function(x) {x^4 -x/6}
+    f(3)
+    f(2)
+    
+  suma <- function(y) {y + 3}
+    suma(3)
+    
+  f2 <- function(x,y) {x+y+2}
+    f2 (1,2)
+
+#Listar variables
+  ls()
+  
+#Borrar variables y funciones
+  rm(variable)
+  
+  rm(list = ls()) #borrar todas las variables y funciones
